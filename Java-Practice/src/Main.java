3eccc3728965ee7c6a26374655cc2b8bdc86cf11
Devin Beliveau <devinmarieb@gmail.com>
2017-05-19 16:06:10 -0600

    Change name

---
+++ Java-Practice/src/Main.java
@@ -0,0 +1,139 @@
+import java.util.Scanner;
+
+//PRINTING OUT PRINT VS PRINTLN
+//public class Main {
+//    public static void main(String args[]) {
+//        double tree;
+//        tree = 5.25;
+//
+//        System.out.print("I want ");
+//        System.out.println(tree);
+//    }
+//}
+
+//VARIABLES
+//class Main {
+//    public static void main(String args[]) {
+//        Scanner devin = new Scanner(System.in);
+//        System.out.println(devin.nextLine());
+//    }
+//}
+
+//CALCULATOR
+//class Main {
+//    public static void main(String args[]) {
+//        Scanner devin = new Scanner(System.in);
+//        double fnum, snum, answer;
+//        System.out.println("Enter first number: ");
+//        fnum = devin.nextDouble();
+//        System.out.println("Enter second number: ");
+//        snum = devin.nextDouble();
+//        answer = fnum + snum;
+//        System.out.println(answer);
+//    }
+//}
+
+//MATH OPERATORS + - * / %
+//class Main {
+//    public static void main(String args[]) {
+//        double girls, boys, people;
+//        girls = 7;
+//        boys = 3;
+//        people = girls / boys;
+//        System.out.println(people);
+//    }
+//}
+
+//INCREMENT VARIABLE
+//class Main {
+//    public static void main(String arg[]) {
+//        int tree = 5;
+//        int willow = 2;
+//        System.out.println(++tree);
+//        System.out.println(tree++);
+//        System.out.println(tree);
+//        System.out.println(+=tree);
+//    }
+//}
+
+//IF STATEMENT
+//class Main {
+//    public static void main(String args[]) {
+//        int test = 9;
+//        if(test == 8) {
+//            System.out.println("Yes");
+//        } else {
+//            System.out.println("No");
+//        }
+//    }
+//}
+
+//LOGICAL OPERATORS && ||
+//class Main {
+//    public static void main(String args[]) {
+//        int boy, girl;
+//        boy = 22;
+//        girl = 29;
+//
+//        if(boy > 18 && girl < 30) {
+//            System.out.println("You can enter");
+//        } else {
+//            System.out.println("You cannot enter");
+//        }
+//    }
+//}
+
+//SWITCH STATEMENT
+//class Main {
+//    public static void main(String args[]) {
+//        int age;
+//        age = 3;
+//
+//        switch(age) {
+//            case 1:
+//                System.out.println("You can crawl");
+//                break;
+//            case 2:
+//                System.out.println("You can talk");
+//                break;
+//            case 3:
+//                System.out.println("You can get in trouble");
+//                break;
+//            default:
+//                System.out.println("You can do everything");
+//                break;
+//        }
+//    }
+//}
+
+//WHILE LOOP
+//class Main {
+//    public static void main(String args[]) {
+//        int counter = 0;
+//        while(counter < 10) {
+//            System.out.println(counter);
+//            counter++;
+//        }
+//    }
+//}
+
+//MULTIPLE CLASSES
+//class Main {
+//    public static void main(String[] args) {
+//       Second secondObject = new Second();
+//       secondObject.simpleMessage();
+//    }
+//}
+
+//METHODS WITH PARAMETERS
+//class Main {
+//    public static void main(String args[]) {
+//        Scanner input = new Scanner(System.in);
+//        Second secondObject = new Second();
+//
+//        System.out.println("Enter name:");
+//        String name = input.nextLine();
+//
+//        secondObject.simpleMessage(name);
+//    }
+//}
---
+++ Java-Practice/src/Second.java
@@ -0,0 +1,5 @@
+public class Second {
+    public void simpleMessage(String name) {
+        System.out.println("Hello " + name);
+    }
+}
