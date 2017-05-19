2a01860e123467f49d4583f3598345aae5bebff1
Devin Beliveau <devinmarieb@gmail.com>
2017-05-19 15:09:45 -0600

    Calculator

--- Java-IdeaBox/src/Main.java
+++ Java-IdeaBox/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//PRINTING OUT PRINT VS PRINTLN
 //public class Main {
 //    public static void main(String args[]) {
 //        double tree;
@@ -10,10 +11,23 @@ import java.util.Scanner;
 //    }
 //}
 
+//VARIABLES
+//class Main {
+//    public static void main(String args[]) {
+//        Scanner devin = new Scanner(System.in);
+//        System.out.println(devin.nextLine());
+//    }
+//}
 
+//CALCULATOR
 class Main {
     public static void main(String args[]) {
         Scanner devin = new Scanner(System.in);
-        System.out.println(devin.nextLine());
+        double fnum, snum, answer;
+        System.out.println("Enter first number: ");
+        fnum = devin.nextDouble();
+        System.out.println("Enter second number: ");
+        snum = devin.nextDouble();
+        System.out.println(fnum + snum);
     }
 }
